46c41532bf93099a8544bc9a4a910f5225659f46
MannShah17 <mannshah2004@gmail.com>
2023-06-14 17:20:44 +0530

    removed

--- models/invoiceModel.js
+++ models/invoiceModel.js
@@ -1,35 +1,14 @@
 const mongoose = require('mongoose');
 
+// customer: This field references the Customer model using the ObjectId type. It establishes a relationship between the Invoice and Customer models, where each invoice is associated with a specific customer. It is a required field.
+// items: This field represents an array of objects, where each object represents an item included in the invoice. Each item object contains the following properties:
+// description: A string describing the item. It is a required field.
+// quantity: The number of units or quantity of the item. It is a required field.
+// price: The price of the item. It is a required field.
+// dueDate: This field represents the due date of the invoice. It is of type Date and is a required field.
+// paymentTerms: This field represents the payment terms or conditions associated with the invoice. It is of type String and is a required field.
 const invoiceSchema = new mongoose.Schema({
-  customer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Customer',
-    required: true,
-  },
-  items: [
-    {
-      description: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
-  dueDate: {
-    type: Date,
-    required: true,
-  },
-  paymentTerms: {
-    type: String,
-    required: true,
-  },
+  //Write a invoice schema here
 });
 
 const Invoice = mongoose.model('Invoice', invoiceSchema);
