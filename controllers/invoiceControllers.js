75367189722d5e0efcf9327350f1bcfdd4543cd7
MannShah17 <mannshah2004@gmail.com>
2023-06-14 17:21:51 +0530

    removed

--- controllers/invoiceControllers.js
+++ controllers/invoiceControllers.js
@@ -5,24 +5,7 @@ const Customer = require('../models/customerModel');
 
 const newInvoice = async (req, res) => {
   try {
-    const { customer, items, dueDate, paymentTerms } = req.body;
-
-    // Find the customer
-    const foundCustomer = await Customer.findById(customer);
-    if (!foundCustomer) {
-      return res.status(404).json({ error: 'Customer not found' });
-    }
-
-    // Create the invoice
-    const invoice = new Invoice({
-      customer,
-      items,
-      dueDate,
-      paymentTerms,
-    });
-    await invoice.save();
-
-    res.status(201).json(invoice);
+    //Write a code here
   } catch (error) {
     res.status(400).json({ error: 'Bad request' });
   }
@@ -30,8 +13,7 @@ const newInvoice = async (req, res) => {
 
 const getAllInvoices = async (req, res) => {
   try {
-    const invoices = await Invoice.find().populate('customer');
-    res.status(201).json(invoices);
+    //Write a code here
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
